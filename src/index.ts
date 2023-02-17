63f42932e841ec2b27fdf42dc0106b1c6f184d33
Junwei Chen <jwchen1259@gmail.com>
2023-02-17 13:16:00 +0900

    feat: add a dedicated logger

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,16 @@ import { nanoid } from "nanoid";
 import { Tetris } from "./core/Tetris.js";
 import { TetrisSocket } from "./websocket/Socket.js";
 import { TetrisSocketServer } from "./websocket/Server.js";
+import logger from "./utils/Logger.js";
 
 dotenv.config();
 
-const server = new TetrisSocketServer({ port: process.env.PORT });
+const server = new TetrisSocketServer({ port: process.env.PORT || 8080 });
 
 server.on("connection", (socket) => {
   const id = nanoid();
 
   server.sockets.set(id, new TetrisSocket(id, socket, new Tetris()));
+
+  logger.info(`[Socket]: Connection established with client (ID: ${id})`);
 });
